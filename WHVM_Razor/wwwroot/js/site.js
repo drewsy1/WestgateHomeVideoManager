ca9a78d106a8b097e551458fd6954dc3b97c8976
Drew Westgate <drew.westgate@outlook.com>
2019-05-13 13:39:52 -0500

    Fixed Sources page datetime selectors

--- WHVM_Razor/wwwroot/js/site.js
+++ WHVM_Razor/wwwroot/js/site.js
@@ -2,3 +2,14 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(function () {
+    const dateFormat = "M/D/YYYY h:mm:ss A";
+
+    $("#dateTimePicker_SourceDateBurned").datetimepicker({
+        format: dateFormat
+    });
+    $("#dateTimePicker_SourceDateRipped").datetimepicker({
+        format: dateFormat
+    });
+});
