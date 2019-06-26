ecbf9ef1fb5421701ea059c35c3246be33b1ae30
Drew Westgate <drew.westgate@outlook.com>
2019-06-25 22:16:37 -0500

    Created ViewExtensions class for later use

---
+++ WHVM_MVC/Classes/ViewExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.ViewEngines;
+using Microsoft.AspNetCore.Routing;
+
+namespace WHVM_MVC.Classes
+{
+    public static class ViewExtensions
+    {
+
+    }
+}
