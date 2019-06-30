d3eef9a64be38e40c471578357103460e22443f9
Drew Westgate <drew.westgate@outlook.com>
2019-06-30 18:11:46 -0500

    Converted Startup Configuration to globally available static variable. Used Startup Configuration to store site name, updated some HTML to use that global variable.

--- WHVM_MVC/Startup.cs
+++ WHVM_MVC/Startup.cs
@@ -17,10 +17,10 @@ namespace WHVM_MVC
     {
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            Startup.Configuration = configuration;
         }
 
-        public IConfiguration Configuration { get; }
+        public static IConfiguration Configuration { get; set; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
