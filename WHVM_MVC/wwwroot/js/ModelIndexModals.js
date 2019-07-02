de00e29408b761837b4da879bbc16e1af946c407
Drew Westgate <drew.westgate@outlook.com>
2019-07-01 23:56:33 -0500

    Moved JS from Sources/Index to ModelIndexModals.js for portability.

---
+++ WHVM_MVC/wwwroot/js/ModelIndexModals.js
@@ -0,0 +1,63 @@
+var detailViewID;
+var detailViewData;
+var progressBar = $('<div/>').attr("class","modal-content").append(
+    $('<div/>').attr("class","modal-body").append(
+        $('<div/>').attr("class","progress").append(
+            $('<div/>').attr({
+                class: "progress-bar progress-bar-striped progress-bar-animated",
+                role: "progressbar",
+                "aria-valuenow": "100",
+                "aria-valuemin": "0",
+                "aria-valuemax": "100",
+                style:"width: 100%"
+            })
+        )
+    )
+)
+
+function SetDetailViewID(id) {
+    detailViewID = id;
+}
+
+function CreateModal(id, isLarge) {
+    var modal = $('<div/>').attr({
+        class: "modal fade",
+        id: id,
+        tabindex: "-1",
+        role: "dialog",
+        "aria-labelledby": id+"Label",
+        "aria-hidden": "true",
+        "data-backdrop": "static"
+    });
+
+    var modalDialog = $('<div/>').attr({
+        class: ((isLarge) ? "modal-dialog modal-lg modal-dialog-centered":"modal-dialog modal-dialog-centered"),
+        role: "document"
+    });
+
+    var modalContent = $('<div/>').attr("class", "modal-content");
+    var modalBody = $('<div/>').attr("class", "modal-body");
+
+    modal.append(modalDialog.append(modalContent.append(modalBody.append(progressBar))));
+
+    return modal;
+}
+
+$('#pageCardBody').on('shown.bs.modal',
+    function (event) {
+        console.log("Showing modal");
+        $.get(detailViewID,
+            function (data) {
+                $(event.target).find('.modal-content').replaceWith(data);
+
+                if ($('#deleteModalBtn').length) {
+                    $('#deleteModalBtn').hide();
+                }
+            });
+    });
+
+$('#pageCardBody').on('hidden.bs.modal',
+    function (event) {
+        console.log("Hiding modal");
+        $(event.target).find('.modal-content').replaceWith(progressBar.clone());
+    });
