0988534396e1bc402497dd7efb61efc4f0abcdf8
Drew Westgate <drew.westgate@outlook.com>
2019-07-12 00:35:40 -0500

    Minor post-merge fix

--- WHVM_MVC/Models/HomeVideoDBContext.cs
+++ WHVM_MVC/Models/HomeVideoDBContext.cs
@@ -69,7 +69,7 @@ namespace WHVM_MVC.Models
                     .HasConstraintName("FK_Clip_TagsPeople_ClipCameraOperator");
 
                 entity.HasOne(d => d.ClipReviewer)
-                    .WithMany(p => p.ClipsAsClipReviewer)
+                    .WithMany(p => p.ClipsAsReviewer)
                     .HasForeignKey(d => d.ClipReviewerId)
                     .HasConstraintName("FK_Clip_TagsPeople_ClipReviewer");
 
